29de9bdc1aa09f3513e2082cc1abf147f469f5a6
Jyoti_Patel <133503718+pateljyoti-4141@users.noreply.github.com>
2023-09-17 02:14:36 +0530

    Add files via upload

---
+++ quote-generator/quotes.js
@@ -0,0 +1,18 @@
+const localQuotes=[
+    {
+        text:'It is never too late to be what you might have been.',
+        author:'George Eliot',
+    },
+    {
+        text:'Pain is inevitable.Suffering is optional.',
+        author:'Haruki Murakami',
+    },
+    {
+        text:'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.',
+        author:'Ralph Waldo Emerson',
+    },
+    {
+        text:'We are all broken, that is how the light gets in.',
+        author:'Ernest Hemingway',
+    }
+]
---
+++ quote-generator/script.js
@@ -0,0 +1,75 @@
+let apiQuotes=[];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader=document.getElementById('loader');
+
+
+//show loading
+function loading(){
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+
+}
+
+//hide loading
+function complete(){
+    quoteContainer.hidden=false;
+    loader.hidden=true;
+}
+
+
+//Show new quote
+function newQuote(){
+    loading();
+    //Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    //check if author field is blank and replace it with 'unknown'
+   if(!quote.author || quote.author == "type.fit"){
+        authorText.textContent = "Unknown";
+    }else{
+        
+        authorText.textContent = quote.author.replace(', type.fit','');
+    }
+
+    //check quote length to determine styling
+    if(quote.text.length>30){
+        quoteText.classList.add('long-quote');
+    }
+    else{
+        quoteText.classList.remove('long-quote');
+    }
+    
+    //set quote,hide loader
+    quoteText.textContent = quote.text;
+    complete();
+   
+}
+
+//get quotes from API
+async function getQuotes(){
+    loading();
+    const apiUrl='https://type.fit/api/quotes';
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes= await response.json();
+        newQuote();
+    }catch(error){
+        
+        
+        //Catch Error Here
+    }
+}
+// Tweet Quote
+function tweetQuote(){
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,'_blank');
+}
+//Event Listeners
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
+//On load
+getQuotes();
